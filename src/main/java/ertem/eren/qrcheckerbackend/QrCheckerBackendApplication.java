92dff6b92c92e3332b2fc68be7ed174b24c122c6
Eren Ertem <mail.erenertem@gmail.com>
2025-01-24 17:55:44 +0300

    initial commit

---
+++ src/main/java/ertem/eren/qrcheckerbackend/QrCheckerBackendApplication.java
@@ -0,0 +1,13 @@
+package ertem.eren.qrcheckerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCheckerBackendApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(QrCheckerBackendApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/ertem/eren/qrcheckerbackend/QrCheckerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ertem.eren.qrcheckerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCheckerBackendApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
